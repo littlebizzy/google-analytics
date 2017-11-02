734414c433a229523ac8df5d8868e97fe3c8760c
Jesse Nickles <jesse@littlebizzy.com>
2017-11-02 09:49:20 -0700

    Update google-analytics.php

--- google-analytics.php
+++ google-analytics.php
@@ -3,23 +3,29 @@
 Plugin Name: Google Analytics
 Plugin URI: https://www.littlebizzy.com/plugins/google-analytics
 Description: Inserts Google Analytics code just above the closing body tag to ensure fastest performance possible and to avoid conflicting with any other scripts.
-Version: 1.0.3
+Version: 1.0.4
 Author: LittleBizzy
 Author URI: https://www.littlebizzy.com
-License: GPL3
+License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
+Prefix: GASLBZ
 */
+
+// Admin Notices module
+require_once dirname(__FILE__).'/admin-notices.php';
+GASLBZ_Admin_Notices::instance(__FILE__);
+
 
 /* Initialization */
 
-// Avoid script calls via plugin URL
+// Block direct calls
 if (!function_exists('add_action'))
 	die;
 
-// This plugin constants
+// Plugin constants
 define('GASLBZ_FILE', __FILE__);
 define('GASLBZ_PATH', dirname(GASLBZ_FILE));
-define('GASLBZ_VERSION', '1.0.2');
+define('GASLBZ_VERSION', '1.0.4');
 
 // Plugin uninstall hook
 register_uninstall_hook(GASLBZ_FILE, 'gaslbz_plugin_uninstall');
@@ -35,9 +41,6 @@ if (is_admin()) {
 		require_once(GASLBZ_PATH.'/admin.php');
 		add_action('admin_menu', array('GASLBZ_Admin', 'admin_menu'));
 	}
-
-	require_once( GASLBZ_PATH . '/admin-suggestions.php' );
-	GASLBZ_Admin_Suggestions::instance();
 
 // Front
 } elseif (!(defined('DOING_CRON') && DOING_CRON)) {
