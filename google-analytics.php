5c3bbe448270650423c663d82a4f4e3c271ca189
Jesse Nickles <jesse@littlebizzy.com>
2018-09-01 02:56:30 -0500

    Update google-analytics.php

--- google-analytics.php
+++ google-analytics.php
@@ -3,7 +3,7 @@
 Plugin Name: Google Analytics
 Plugin URI: https://www.littlebizzy.com/plugins/google-analytics
 Description: Inserts Google Analytics code just above the closing body tag to ensure fastest performance possible and to avoid conflicting with any other scripts.
-Version: 1.0.6
+Version: 1.0.7
 Author: LittleBizzy
 Author URI: https://www.littlebizzy.com
 License: GPLv3
@@ -31,7 +31,7 @@ if (!function_exists('add_action'))
 // Plugin constants
 define('GASLBZ_FILE', __FILE__);
 define('GASLBZ_PATH', dirname(GASLBZ_FILE));
-define('GASLBZ_VERSION', '1.0.6');
+define('GASLBZ_VERSION', '1.0.7');
 
 // Plugin uninstall hook
 register_uninstall_hook(GASLBZ_FILE, 'gaslbz_plugin_uninstall');
